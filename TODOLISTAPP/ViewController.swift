8035b08a9a4123d71d6baceb8ff21642868ce759
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2023-07-18 14:43:05 +0200

    Initial commit

---
+++ TODOLISTAPP/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TODOLISTAPP
+//
+//  Created by Reuben Simphiwe Kuse on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
